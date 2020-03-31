963c5a26fcd80ba3a230c52e2f288066f831f02a
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-03-31 16:05:58 +0900

    UIPinchGestureRecognizerSample

--- UIPinchGestureRecognizerSample/ViewController.swift
+++ UIPinchGestureRecognizerSample/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pinchLabel(_ sender: UIPinchGestureRecognizer) {
+        label.transform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
     }
 
-
 }
-
